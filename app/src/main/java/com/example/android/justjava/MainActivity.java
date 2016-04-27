b01665ca8207f3f5fa0c09e306eddb70a3f31872
Ivan <ivancondori1@gmail.com>
2016-04-27 12:08:11 -0500

    PrecioBase

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -51,17 +51,34 @@ public class MainActivity extends AppCompatActivity {
         Log.v("MainActivity","Has Chocolate: "+hasChocolate);
 
 
-        int price=calculatePrice();
+        int price=calculatePrice(hasWhippedCream,hasChocolate);
         String priceMessage = createOrderSummary(price,hasWhippedCream,hasChocolate,name);
         displayMessage(priceMessage);
     }
 
     /**
      * Calculates the price of the order
+     * @param addWhippedCream is whether or not the user whipped cream topping
+     * @param addChocolate is whether or not the user wants chocolate topping
      * @return total price
+     *
      * */
-    private int calculatePrice(){
-        return quantity*5;
+    private int calculatePrice(boolean addWhippedCream,boolean addChocolate) {
+        //Price of 1 cup of coffee
+        int basePrice=5;
+
+        //Add $1 if the user wants whipped cream
+        if(addWhippedCream){
+            basePrice = basePrice+1;
+        }
+
+        //Add $2 if the user wants chocolate
+        if(addChocolate){
+            basePrice = basePrice+2;
+        }
+
+        //Calculate the total order price by multiplying
+        return quantity*basePrice;
     }
 
 
